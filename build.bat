806a867113e7802b94b3fe233a2e79b639440f44
ragnarlodbrok1992 <maraus92@gmail.com>
2024-01-01 17:34:02 +0100

    WIP - initial commit

---
+++ build.bat
@@ -0,0 +1,14 @@
+@echo off
+SET compiler_dir="C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.31.31103\bin\Hostx64\x64\cl.exe"
+
+echo Building...
+
+echo Creating build folder...
+if not exist "build" mkdir build
+
+%compiler_dir% /Ehsc /Zi^
+  /Fe:"build\main.exe"^
+  src\main.cpp^
+  /Fo:"build\main.obj"^
+  /Fd:"build\main.pdb"
+
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("SmolNG\n");
+
+  return 0;
+}
