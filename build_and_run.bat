2bd8892460f966cdaf394ad60253a0725e0c51b6
ragnarlodbrok1992 <maraus92@gmail.com>
2024-01-06 22:24:19 +0100

    WIP - window ready

--- build_and_run.bat
+++ build_and_run.bat
@@ -1,6 +1,9 @@
 @echo off
 SET compiler_dir="C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.31.31103\bin\Hostx64\x64\cl.exe"
+
 SET GLAD_INCLUDE="D:\libraries\glad\include"
+SET GLAD_LIB_SRC="D:\libraries\glad\src\glad.c"
+
 SET GLFW_INCLUDE="D:\libraries\glfw-3.3.8\include"
 SET GLFW_LIB="D:\libraries\glfw-3.3.8\build\x64\lib\glfw3_mt.lib"
 
@@ -9,16 +12,19 @@ echo Building...
 echo Creating build folder...
 if not exist "build" mkdir build
 
+pushd build
+
 %compiler_dir% /EHsc /Zi^
-  /Fe:"build\main.exe"^
-  src\main.cpp^
+  /Fe:"main.exe"^
+  ..\src\main.cpp^
+  %GLAD_LIB_SRC%^
   /I%GLAD_INCLUDE%^
   /I%GLFW_INCLUDE%^
-  /Fo:"build\main.obj"^
-  /Fd:"build\main.pdb"^
   /link %GLFW_LIB% OpenGL32.lib Gdi32.lib User32.lib Shell32.lib
 
 echo "Done building!"
+
+popd
 
 echo Running...
 
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,20 @@
  * and Game Engine Architecture by Jason Gregory.
  */
 
+const int WIDTH = 1280;
+const int HEIGHT = 720;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void ProcessInput(GLFWwindow* window) {
+  // Closing window using ESC key
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
 int main(void) {
   // Intro message
   printf("SmolNG\n");
@@ -17,6 +31,46 @@ int main(void) {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   
+  // Window creation
+  GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "SmolNG", NULL, NULL);
+  if (window == NULL) {
+    printf("Failed to create GLFW window\n");
+    glfwTerminate();
+    return -1;
+  }
+
+  // Make the context of our window the main context on the current thread
+  glfwMakeContextCurrent(window);
+
+  // Initialize GLAD
+  if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+    printf("Failed to initialize GLAD\n");
+    return -1;
+  }
+
+  // Is this working?
+  // gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+  // gladLoadGL();
+
+  // Set viewport
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // Main loop
+  while(!glfwWindowShouldClose(window)) {
+    // Input
+    ProcessInput(window);
+
+    // Rendering commands
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT); 
+
+    // Check and call events and swap buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  // Terminate GLFW
+  glfwTerminate();
 
   // End message - if it shows, everything went well
   printf("Everything went well, exiting...\n");
